c965a66012414913b4b90cca9235ec3fb41d1901
creator-m1r <creator.in.the.heart@gmail.com>
2026-08-17 14:53:55 -0700

    Add Swift package manifest

---
+++ Package.swift
@@ -0,0 +1,9 @@
+// swift-tools-version: 5.10
+import PackageDescription
+
+let package = Package(
+    name: "MIR4DPatentSourceListing",
+    platforms: [.macOS(.14)],
+    products: [.executable(name: "MIR4DPatentSourceListing", targets: ["MIR4DPatentSourceListing"])],
+    targets: [.executableTarget(name: "MIR4DPatentSourceListing", path: "Sources/MIR4DPatentSourceListing")]
+)
