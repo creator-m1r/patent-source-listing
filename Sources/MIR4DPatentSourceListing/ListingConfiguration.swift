af052da7d0a2bfa6d99b7cfebaa008baab5e5338
creator-m1r <creator.in.the.heart@gmail.com>
2026-08-18 09:07:18 -0700

    Add configurable listing options model

---
+++ Sources/MIR4DPatentSourceListing/ListingConfiguration.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+struct ListingConfiguration: Codable {
+    var includeExtensions: Set<String> = []
+    var excludedDirectories: Set<String> = [".git", ".github", ".build", "build", "Build", "DerivedData", "Pods", "node_modules", ".swiftpm", "xcuserdata", "dist", "Debug", "Release", "__pycache__", ".idea", ".vscode"]
+    var excludedFileNames: Set<String> = ["Package.resolved"]
+    var includeDocumentation: Bool = false
+    var includeConfigurationFiles: Bool = false
+    var includeEmptyFiles: Bool = true
+    var fontName: String = "Courier New"
+    var fontSize: Double = 10
+    var separatorWidth: Int = 80
+    var logicalOrder: Bool = true
+    var pageNumbers: Bool = true
+    var outputSizeLimitMB: Int = 5
+
+    static let `default` = ListingConfiguration()
+}
