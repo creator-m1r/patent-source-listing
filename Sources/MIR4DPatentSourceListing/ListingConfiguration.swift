cf045faf58ce5fff0ec0f88a92d9da6af9aaa2ed
creator-m1r <creator.in.the.heart@gmail.com>
2026-08-22 02:43:34 -0700

    Улучшить конфигурацию патентного листинга и лимит 4,9 МБ

--- Sources/MIR4DPatentSourceListing/ListingConfiguration.swift
+++ Sources/MIR4DPatentSourceListing/ListingConfiguration.swift
@@ -2,8 +2,12 @@ import Foundation
 
 struct ListingConfiguration: Codable {
     var includeExtensions: Set<String> = []
-    var excludedDirectories: Set<String> = [".git", ".github", ".build", "build", "Build", "DerivedData", "Pods", "node_modules", ".swiftpm", "xcuserdata", "dist", "Debug", "Release", "__pycache__", ".idea", ".vscode"]
-    var excludedFileNames: Set<String> = ["Package.resolved"]
+    var excludedDirectories: Set<String> = [
+        ".git", ".github", ".build", "build", "Build", "DerivedData",
+        "Pods", "node_modules", ".swiftpm", "xcuserdata", "dist",
+        "Debug", "Release", "__pycache__", ".idea", ".vscode"
+    ]
+    var excludedFileNames: Set<String> = ["Package.resolved", ".DS_Store"]
     var includeDocumentation: Bool = false
     var includeConfigurationFiles: Bool = false
     var includeEmptyFiles: Bool = true
@@ -13,7 +17,8 @@ struct ListingConfiguration: Codable {
     var separatorWidth: Int = 80
     var logicalOrder: Bool = true
     var pageNumbers: Bool = true
-    var outputSizeLimitMB: Int = 5
+    /// Точный предельный размер готового RTF в байтах. 4,9 МБ = 4 900 000 байт.
+    var outputSizeLimitBytes: Int = 4_900_000
 
     static let `default` = ListingConfiguration()
 }
